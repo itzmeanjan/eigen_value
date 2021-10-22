6d2cd8c78d111ab6a803562a0f3d8fe1f19256a0
Anjan Roy <anjanroy@yandex.com>
2021-10-22 07:15:07 +0000

    generate vector for testing max value finder function

--- include/utils.hpp
+++ include/utils.hpp
@@ -4,3 +4,6 @@ void identity_matrix(sycl::queue &q, float *const mat, const uint dim,
                      const uint wg_size);
 
 void check(const float *vec, const uint dim);
+
+void generate_vector(sycl::queue &q, float *const vec, const uint dim,
+                     const uint wg_size);
--- utils.cpp
+++ utils.cpp
@@ -26,3 +26,24 @@ void check(const float *vec, const uint dim) {
     assert(vec[i] == 1.f);
   }
 }
+
+void generate_vector(sycl::queue &q, float *const vec, const uint dim,
+                     const uint wg_size) {
+  memset(vec, 0, sizeof(float) * dim);
+
+  sycl::buffer<float, 1> b_vec{vec, sycl::range<1>{dim}};
+
+  q.submit([&](sycl::handler &h) {
+    sycl::accessor<float, 1, sycl::access::mode::write,
+                   sycl::access::target::global_buffer>
+        acc_vec{b_vec, h};
+
+    h.parallel_for(
+        sycl::nd_range<1>{sycl::range<1>{dim}, sycl::range<1>{wg_size}},
+        [=](sycl::nd_item<1> it) {
+          const size_t r = it.get_global_id(0);
+
+          acc_vec[r] = r + 1;
+        });
+  });
+}
