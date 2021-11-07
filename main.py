dc0228df86b0976e7a3906e6b85919a76c742f85
Anjan Roy <anjanroy@yandex.com>
2021-11-07 07:25:55 +0000

    run benchmarks on sequential python implementation of similarity transform

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 import numpy as np
-from functools import reduce
+from time import time
 
-EPS = 1e-5
+EPS = 1e-3
 
 '''
     Sequential Implementation of https://doi.org/10.1007/978-3-319-11194-0_18
@@ -30,20 +30,41 @@ def stop(vec):
 def max_eigen_value_and_vector(mat):
     eigen_val = 0
     eigen_vec = np.ones(mat.shape[0])
+
+    itr = 0
     while True:
         vec = sum_across_rows(mat)
-        eigen_vec = np.array([j * (vec[i]/np.max(vec))
+        vec_max = np.max(vec)
+        eigen_vec = np.array([j * (vec[i]/vec_max)
                               for i, j in enumerate(eigen_vec)])
         if stop(vec):
             eigen_val = vec[0]
             break
 
         mat = compute_next(mat, vec)
+        itr += 1
 
-    return eigen_val, eigen_vec
+    return eigen_val, eigen_vec, itr + 1
 
 
 if __name__ == '__main__':
+    # handwritten test begins
     mat = np.array([[1, 1, 2], [2, 1, 3], [2, 3, 5]])
-    val, vec = max_eigen_value_and_vector(mat)
-    print(f'eigen value: {val}\neigen vector: {vec}')
+    val, vec, _ = max_eigen_value_and_vector(mat)
+
+    assert abs(val - 7.5311) < EPS
+    assert abs(vec[0] - 0.3941) < EPS
+    assert abs(vec[1] - 0.5788) < EPS
+    assert abs(vec[2] - 0.9975) < EPS
+    # handwritten test ends
+
+    print('Sequential Similarity Transform, for finding maximum eigen value ( with vector )\n')
+    for dim in range(5, 11):
+        mat = np.random.random((1 << dim, 1 << dim))
+        start = time() * 1000
+        val, _, itr = max_eigen_value_and_vector(mat)
+        end = time() * 1000
+
+        assert val - np.max(np.linalg.eigvals(mat)) < EPS
+        print(
+            f'{1 << dim:<4} x {1 << dim:>4}\t\t{end - start:>6.2f} ms\t\t{itr:>8} round(s)')
