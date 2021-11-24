930117d64c45f71bbb1c27b9decde7eb2539d995
Anjan Roy <anjanroy@yandex.com>
2021-11-24 15:59:34 +0000

    max work group size bounded dynamic work group size setting

--- main.cpp
+++ main.cpp
@@ -5,27 +5,31 @@
 
 using namespace sycl;
 
-const uint N = 1 << 13;
-const uint B = 1 << 5;
-
 int main() {
   device d{default_selector{}};
   queue q{d};
+
+  const size_t max_wg_size =
+      d.get_info<info::device::max_work_group_size>() >> 1;
+
   std::cout << "running on " << d.get_info<info::device::name>() << "\n"
             << std::endl;
   std::cout << "Parallel Algorithm using Similarity Transform for finding max "
                "eigen value (with vector)\n"
             << std::endl;
 
-  for (uint dim = B; dim <= N; dim <<= 1) {
+  for (uint i = 7; i <= 13; i++) {
+    const uint dim = 1ul << i;
+
     float *mat = (float *)malloc(sizeof(float) * dim * dim);
     float *eigen_val = (float *)malloc(sizeof(float) * 1);
     float *eigen_vec = (float *)malloc(sizeof(float) * dim * 1);
 
     generate_hilbert_matrix(q, mat, dim);
     uint itr_count = 0;
-    int64_t tm =
-        similarity_transform(q, mat, eigen_val, eigen_vec, dim, B, &itr_count);
+    int64_t tm = similarity_transform(q, mat, eigen_val, eigen_vec, dim,
+                                      dim <= max_wg_size ? dim : max_wg_size,
+                                      &itr_count);
 
     std::cout << std::setw(5) << std::left << dim << "x" << std::setw(5)
               << std::right << dim << "\t\t\t" << std::setw(10) << std::right
