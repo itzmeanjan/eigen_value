466c7522486fc54241debb73abbe2a9f67625d21
Anjan Roy <anjanroy@yandex.com>
2022-01-11 15:52:17 +0000

    updated c shared library api to cretae sycl queue once and then pass it to maximum eigen value finder function

--- wrapper/similarity_transform.cpp
+++ wrapper/similarity_transform.cpp
@@ -1,19 +1,31 @@
 #include "similarity_transform.hpp"
 
+extern "C" void
+make_queue(void** wq)
+{
+  sycl::default_selector d_sel{};
+  sycl::device d{ d_sel };
+  sycl::context c{ d };
+  sycl::queue* q = new sycl::queue{ c, d };
+
+  *wq = q;
+}
+
 extern "C" int64_t
-max_eigen_value(float* mat,
+max_eigen_value(void* wq,
+                float* mat,
                 float* eigen_val,
                 float* eigen_vec,
                 uint dim,
                 uint* iter_cnt)
 {
-  sycl::device d{ sycl::default_selector{} };
-  sycl::queue q{ d };
+  sycl::queue* q = reinterpret_cast<sycl::queue*>(wq);
+
   const size_t max_wg_size =
-    d.get_info<sycl::info::device::max_work_group_size>();
+    q->get_device().get_info<sycl::info::device::max_work_group_size>();
 
   int64_t ts =
-    similarity_transform(q,
+    similarity_transform(*q,
                          mat,
                          eigen_val,
                          eigen_vec,
